8550bfc19fd607f156cd4391e2621ec999edfd5d
kamal <tekkensingle@gmail.com>
2023-12-14 20:22:11 +0530

    make method public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,14 +12,14 @@ public static void main(String args[]){
 
     }
    public static class A{
-        String meth(){
+        public String meth(){
             return "Invoking method from class A";
         }
     }
 
     public static class B extends A{
-
-        String meth(){
+    @Override
+      public  String meth(){
 
             return "Method is overridden in Extendend class B";
         }

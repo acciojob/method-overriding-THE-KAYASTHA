2b935defd857923450880e61a829c46800c36a1f
kamal <tekkensingle@gmail.com>
2023-12-14 19:49:28 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+
+public static void main(String args[]){
+
+
+     B one=new B();
+    String a= one.meth();
+     String b=one.meth();
+     System.out.println(a);
+    System.out.println(b);
+    }
+
   
+}
+class A{
+      String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    @Override
+    String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+
 }
